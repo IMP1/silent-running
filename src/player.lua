1b3ac49546d8be8c3bd8975aff5078444cfda640
Huw Taylor <huw_taylor@hotmail.co.uk>
2017-01-19 22:14:07 +0000

    added epsilon for slow moment

--- src/player.lua
+++ src/player.lua
@@ -1,8 +1,9 @@
 local Player = {}
 Player.__index = Player
 
-Player.ACCELLERAION = 256
+Player.ACCELLERATION = 256
 Player.FRICTION = 0.99
+Player.EPSILON = 0.5
 
 function Player.new(x, y)
     local this = {}
@@ -30,18 +31,27 @@ function Player:update(dt)
         dx = dx / ROOT_2
         dy = dy / ROOT_2
     end
-    self.vel.x = self.vel.x + dx * Player.ACCELLERAION ^ dt
-    self.vel.y = self.vel.y + dy * Player.ACCELLERAION ^ dt
+    self.vel.x = self.vel.x + dx * Player.ACCELLERATION ^ dt
+    self.vel.y = self.vel.y + dy * Player.ACCELLERATION ^ dt
 
     self.pos.x = self.pos.x + self.vel.x * dt
     self.pos.y = self.pos.y + self.vel.y * dt
 
     self.vel.x = self.vel.x * Player.FRICTION
+    if math.abs(self.vel.x) < Player.EPSILON then
+        self.vel.x = 0
+    end
     self.vel.y = self.vel.y * Player.FRICTION
+    if math.abs(self.vel.y) < Player.EPSILON then
+        self.vel.y = 0
+    end
 end
 
 function Player:draw()
     love.graphics.rectangle("fill", self.pos.x - 32, self.pos.y - 16, 64, 32)
+
+    love.graphics.print(tostring(self.vel.x), 0, 0)
+    love.graphics.print(tostring(self.vel.y), 0, 16)
 end
 
 return Player
