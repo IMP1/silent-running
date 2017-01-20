4defce914e937685d73837efb1a3f15adcfd12d7
IMP1 <IMP1@mail.com>
2017-01-20 16:27:36 +0000

    added rock class, which deals with its triangles. currently only works for convex polygons

---
+++ src/rock.lua
@@ -0,0 +1,29 @@
+local Rock = {}
+Rock.__index = Rock
+
+function Rock.new(...)
+    local this = {}
+    setmetatable(this, Rock)
+    this.polygon = {...}
+    this.triangles = {}
+    for i = 3, #this.polygon - 3, 2 do
+        table.insert(this.triangles, {
+            this.polygon[1], this.polygon[2], 
+            this.polygon[i], this.polygon[i+1],
+            this.polygon[i+2], this.polygon[i+3],
+        })
+    end
+    return this
+end
+
+function Rock:containsPoint(x, y)
+-- http://stackoverflow.com/questions/2049582/how-to-determine-if-a-point-is-in-a-2d-triangle/2049712#2049712
+    return false
+end
+
+function Rock:drawDebug()
+    love.graphics.polygon("fill", self.polygon)
+end
+
+return Rock
+
