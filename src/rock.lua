2a8e831210815273a9eb9a32e8efa189299aa5af
Huw Taylor <huw_taylor@hotmail.co.uk>
2017-04-19 19:01:08 +0100

    fixed collision bug

--- src/rock.lua
+++ src/rock.lua
@@ -23,19 +23,18 @@ function Rock:containsPoint(x, y)
         local s = r[2] * r[5] - r[1] * r[6] + (r[6] - r[2]) * x   + (r[1] - r[5]) * y
      -- local t = p0.X * p1.Y - p0.Y * p1.X + (p0.Y - p1.Y) * p.X + (p1.X - p0.X) * p.Y;
         local t = r[1] * r[4] - r[2] * r[3] + (r[2] - r[4]) * x   + (r[3] - r[1]) * y
-        if ((s < 0) ~= (t < 0)) then
-            return false
-        end
-     -- local A = -p1.Y * p2.X + p0.Y * (p2.X - p1.X) + p0.X * (p1.Y - p2.Y) + p1.X * p2.Y;
-        local A = -r[4] * r[5] + r[2] * (r[5] - r[3]) + r[1] * (r[4] - r[6]) + r[3] * r[6]
-        
-        if (A < 0.0) then
-            s = -s
-            t = -t
-            A = -A
-        end
-        if s > 0 and t > 0 and (s + t) <= A then
-            return true
+        if not ((s < 0) ~= (t < 0)) then
+         -- local A = -p1.Y * p2.X + p0.Y * (p2.X - p1.X) + p0.X * (p1.Y - p2.Y) + p1.X * p2.Y;
+            local A = -r[4] * r[5] + r[2] * (r[5] - r[3]) + r[1] * (r[4] - r[6]) + r[3] * r[6]
+            
+            if (A < 0.0) then
+                s = -s
+                t = -t
+                A = -A
+            end
+            if s > 0 and t > 0 and (s + t) <= A then
+                return true
+            end
         end
     end
     return false
