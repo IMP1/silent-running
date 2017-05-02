5cba6b26a42736b449a2a39d7c4826f6c37a2705
Huw Taylor <huw_taylor@hotmail.co.uk>
2017-05-02 20:47:26 +0100

    logging can be turned off

--- src/log.lua
+++ src/log.lua
@@ -1,3 +1,5 @@
+local DISABLED = false
+
 local Log = {}
 Log.__index = Log
 
@@ -9,17 +11,20 @@ function Log.new()
 end
 
 function Log:add(message)
+    if DISABLED then return end
     print(message)
     table.insert(self.messages, message)
 end
 
 function Log:update()
+    if DISABLED then return end
     if (#self.messages + 1) * 16 > love.graphics.getHeight() then
         table.remove(self.messages, 1)
     end
 end
 
 function Log:draw()
+    if DISABLED then return end
     for i, message in pairs(self.messages) do
         love.graphics.printf(message, 0, (i-1) * 16, love.graphics.getWidth(), "right")
     end
