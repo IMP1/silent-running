93ce9fa58619c9ed8c1fbeda9c61c5e576780487
IMP1 <IMP1@mail.com>
2017-05-09 14:23:05 +0100

    added a lobby object, just need to make it a thing

--- src/main.lua
+++ src/main.lua
@@ -23,6 +23,7 @@ local sock   = require 'lib.sock'
 -------------
 -- Classes --
 -------------
+local UiCollection   = require 'mortar'
 local Server         = require 'server'
 local Client         = require 'client'
 local Log            = require 'log'
@@ -33,29 +34,43 @@ function love.load(args)
         role = Server.new()
     end
     if args[2] == "client" then
-        role = Client.new()
+        role = Client.new(args[3])
+    end
+    if not role then 
+        setupLobby()
     end
 end
 
+function setupLobby()
+    lobby = love.filesystem.load("title_layout.lua")()
+end
+
 function love.keypressed(key, isRepeat)
-    if not role then 
+    if lobby then 
         if key == "1" then
             role = Server.new()
         elseif key == "2" then
             role = Client.new("localhost")
         end
-    else
+    end
+    if role then
         role:keypressed(key, isRepeat)
     end
 end
 
 function love.mousepressed(mx, my, key)
+    if lobby then
+        lobby:mousepressed(mx, my, key)
+    end
     if role then
         role:mousepressed(mx, my, key)
     end
 end
 
 function love.update(dt)
+    if lobby then
+        lobby:update(dt)
+    end
     if role then
         role:update(dt)
     end
@@ -76,7 +91,7 @@ end
 
 function drawLobby()
     love.graphics.setColor(255, 255, 255)
-    love.graphics.printf("WAITING", 0, 96, love.graphics.getWidth(), "center")
+    lobby:draw()
 end
 
 function drawDebug()
