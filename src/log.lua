877fff91a0cb9ea84ca9fbe767e26e00772cb6c8
IMP1 <IMP1@mail.com>
2017-05-09 14:21:55 +0100

    added specifying a font

--- src/log.lua
+++ src/log.lua
@@ -3,9 +3,10 @@ local DISABLED = false
 local Log = {}
 Log.__index = Log
 
-function Log.new()
+function Log.new(font)
     local this = {}
     setmetatable(this, Log)
+    this.font = font
     this.messages = {}
     return this
 end
@@ -25,8 +26,16 @@ end
 
 function Log:draw()
     if DISABLED then return end
+    local oldFont = nil
+    if self.font then
+        oldFont = love.graphics.getFont()
+        love.graphics.setFont(self.font)
+    end
     for i, message in pairs(self.messages) do
         love.graphics.printf(message, 0, (i-1) * 16, love.graphics.getWidth(), "right")
+    end
+    if oldFont then
+        love.graphics.setFont(oldFont)
     end
 end
 
